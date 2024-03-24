3e77dfff3dc29fb49b3b00110576e3bc8be316a3
Tuzu <dkamicus@gmail.com>
2024-03-24 17:16:05 +0100

    Drawer

---
+++ Script/Drawer.gd
@@ -0,0 +1,48 @@
+class_name Drawer extends Node2D
+
+enum FieldColor {NONE, CYAN, BLUE, ORANGE, YELLOW, GREEN, PURPLE, RED}
+const ColorMap = ['#FFFFFF', '#00FFFF', '#0476D0', '#FFA500', '#FDFD96', '#77DD77', '#8F00FF', '#FF033E']
+
+var xCount = 10
+var yCount = 20
+var tileSize = 8
+var backgroundTileTexture : Texture2D
+var tileTexture : Texture2D
+var fields : Array
+
+func _init(
+		AXCount, AYCount: int,
+		ABackgroundTileTexture, ATileTexture: Texture2D,
+		fieldArr: Array
+	):
+	xCount = AXCount
+	yCount = AYCount
+	backgroundTileTexture = ABackgroundTileTexture
+	tileTexture = ATileTexture
+	fields = fieldArr
+	pass
+
+func _ready():
+	tileSize = min(backgroundTileTexture.get_width(), backgroundTileTexture.get_height())
+	pass
+	
+func draw(x, y: int):
+	if fields[y][x] == FieldColor.NONE:
+		draw_texture(backgroundTileTexture, Vector2(tileSize * x, tileSize * y))
+	else:
+		draw_texture(tileTexture, Vector2(tileSize * x, tileSize * y), ColorMap[fields[y][x]])
+
+func _draw():
+	for y in yCount:
+		for x in xCount:
+			draw(x, y)
+
+
+
+
+
+
+
+
+
+
